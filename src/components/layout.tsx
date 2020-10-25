bf56fc90c0e6b3c4f7df60b9bc3304be2256d038
@erick <erick.hx22@gmail.com>
2020-10-25 11:56:24 -0500

    refactor: rename import for folder aliases (#3)

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,15 +1,7 @@
-/**
- * Layout component that queries for data
- * with Gatsby's useStaticQuery component
- *
- * See: https://www.gatsbyjs.com/docs/use-static-query/
- */
-
 import React from "react"
 import PropTypes from "prop-types"
 import { useStaticQuery, graphql } from "gatsby"
-
-import Header from "./header"
+import Header from '@components/header'
 import "./layout.scss"
 
 const Layout: React.FC = ({ children }) => {
