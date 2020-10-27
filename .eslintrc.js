68dd937cbc2fcb54e2a4d3974c8a6d0379e1d737
@erick <erick.hx22@gmail.com>
2020-10-26 23:30:10 -0500

    feat: implement some rules to .eslintrc (#9)

--- .eslintrc.js
+++ .eslintrc.js
@@ -21,5 +21,18 @@ module.exports = {
   plugins: ["react", "@typescript-eslint", "prettier"],
   rules: {
     "prettier/prettier": "error",
+    "import/prefer-default-export": "off",
+    "no-use-before-define": "off",
+    "react/prop-types": ["error", { ignore: ["children", "props.children"] }],
+    "react/require-default-props": "off",
+    "react/jsx-filename-extension": [
+      2,
+      { extensions: [".js", ".jsx", ".ts", ".tsx"] },
+    ],
+  },
+  settings: {
+    "import/resolver": {
+      typescript: {},
+    },
   },
 }
