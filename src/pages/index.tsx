ae440cb5695af7a5c67efcc3e64e6c996aff6845
@erick <erick.hx22@gmail.com>
2020-10-31 19:23:00 -0500

    chore: implement banner to home page (#13)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,12 @@
-import React from "react"
-import { Link } from "gatsby"
-
+import { Banner } from "@components/Banner"
 import { Layout } from "@components/Layout"
-import Image from "@components/image"
 import SEO from "@components/seo"
+import React from "react"
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
-    <br />
-    <Link to="/using-typescript/">Go to &ldquo;Using TypeScript&quot;</Link>
+    <Banner />
   </Layout>
 )
 
