b0d6b3a089672153de94c9e3d558a69dddc6ba36
@erick <erick.hx22@gmail.com>
2020-10-31 19:21:14 -0500

    chore: implement banner component (#13)

---
+++ src/components/Banner/Banner.tsx
@@ -0,0 +1,21 @@
+/* eslint-disable react/no-unescaped-entities */
+import React from "react"
+import "./banner.scss"
+
+export const Banner: React.FC = () => (
+  <section className="Banner">
+    <div className="Banner__container">
+      <h2 className="Banner__green">Hi, my name is</h2>
+      <h2 className="Banner__name">Erick Escriba.</h2>
+      <h2 className="Banner__main__text">I build things for the web.</h2>
+      <p className="Banner__main__description">
+        I'm a front-end developer based in Lima, Peru . I'm currently into Web
+        Development and I'm passionate in building (and occasionally designing)
+        exceptional websites
+      </p>
+      <div className="Banner__contact-btn">
+        <a href="mailto:erickescribaa@gmail.com">Get In Touch</a>
+      </div>
+    </div>
+  </section>
+)
