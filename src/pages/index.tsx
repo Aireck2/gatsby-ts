f0ff9547c843376ee55c618e41138f54ebe00531
@erick <erick.hx22@gmail.com>
2020-11-11 19:37:15 -0500

    refactor: implement global styles to style folder (#18)

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Banner } from "@components/Banner"
 import { Layout } from "@components/Layout"
 import SEO from "@components/seo"
 import React from "react"
+import "../style/globalStyles.scss"
 
 const IndexPage = () => (
   <Layout>
