c896815263c54618a0cf37cb38800fb3c907da49
@erick <erick.hx22@gmail.com>
2020-11-21 13:17:39 -0500

    feat: implement default export for icon logo(#22)

--- src/components/Icons/index.ts
+++ src/components/Icons/index.ts
@@ -1,2 +1,4 @@
+/* eslint-disable import/extensions */
 export { default as IconGitHub } from "./github"
+export { default as IconLogo } from "./iconLogo"
 export { default as IconLinkedin } from "./linkedin"
