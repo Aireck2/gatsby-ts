e2c927cb89b0198cb8120baedf2d0b2b505ed85e
@erick <erick.hx22@gmail.com>
2020-11-21 12:14:07 -0500

    feat: implement styles to 404 page (#22)

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,13 +1,14 @@
-import React from "react"
-
 import { Layout } from "@components/Layout"
 import SEO from "@components/seo"
+import React from "react"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404 Not found" />
-    <h1>404: Not Found</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <section className="Banner">
+      <h1>404: Not Found</h1>
+      <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </section>
   </Layout>
 )
 
