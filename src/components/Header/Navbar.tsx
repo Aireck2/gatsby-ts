76d6419cf138bb1bae2db357bc8ddc26d98f9364
@erick <erick.hx22@gmail.com>
2020-11-21 12:16:44 -0500

    chore: implement navbar for menu mobile (#22)

--- src/components/Header/Navbar.tsx
+++ src/components/Header/Navbar.tsx
@@ -1,32 +1,36 @@
+import { Button } from "@components/Button"
+import { Menu } from "@components/Header"
 import { Link } from "gatsby"
 import React from "react"
-import { Button } from "../Button"
 import "./navbar.scss"
 
 export const Navbar: React.FC = () => {
   return (
-    <div className="Navbar">
-      <ul className="Navbar__container">
-        <li className="Navbar__item">
-          <span className="Navbar__item__number">01.</span>
-          <span className="Navbar__item__string">
-            <Link to="/about">About</Link>
-          </span>
-        </li>
-        <li className="Navbar__item">
-          <span className="Navbar__item__number">02.</span>
-          <span className="Navbar__item__string">
-            <Link to="/work">Work</Link>
-          </span>
-        </li>
-        <li className="Navbar__item">
-          <span className="Navbar__item__number">03.</span>
-          <span className="Navbar__item__string">
-            <Link to="/contact">Contact</Link>
-          </span>
-        </li>
-      </ul>
-      <Button href="/something">Resume</Button>
-    </div>
+    <>
+      <div className="Navbar">
+        <ul className="Navbar__container">
+          <li className="Navbar__item">
+            <span className="Navbar__item__number">01.</span>
+            <span className="Navbar__item__string">
+              <Link to="/#about">About</Link>
+            </span>
+          </li>
+          <li className="Navbar__item">
+            <span className="Navbar__item__number">02.</span>
+            <span className="Navbar__item__string">
+              <Link to="/#work">Work</Link>
+            </span>
+          </li>
+          <li className="Navbar__item">
+            <span className="Navbar__item__number">03.</span>
+            <span className="Navbar__item__string">
+              <Link to="/#contact">Contact</Link>
+            </span>
+          </li>
+        </ul>
+        <Button href="/pdf">Resume</Button>
+      </div>
+      <Menu />
+    </>
   )
 }
