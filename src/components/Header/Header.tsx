8948c07a81a03a38b2357385a1ea2820b3a716ad
@erick <erick.hx22@gmail.com>
2020-12-27 12:19:32 -0500

    chore: repair header position fixed (#28)

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,18 +1,47 @@
 import { Navbar } from "@components/Header"
 import { IconLogo } from "@components/Icons"
+import useScrollDirection from "@src/hooks/useScrollDirection"
 import { Link } from "gatsby"
-import React from "react"
+import React, { useEffect, useState } from "react"
 import "./header.scss"
 
-export const Header: React.FC = () => (
-  <header className="Header">
-    <div className="Header__container">
-      <h1 className="Header__logo">
-        <Link className="Logo" to="/">
-          <IconLogo />
-        </Link>
-      </h1>
-      <Navbar />
-    </div>
-  </header>
-)
+export const Header: React.FC = () => {
+  const scrollDirection = useScrollDirection({ initialDirection: "down" })
+
+  const [scrolledToTop, setScrolledToTop] = useState(true)
+
+  const handleScroll = () => {
+    setScrolledToTop(window.pageYOffset < 50)
+  }
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      // setIsMounted(true)
+    }, 100)
+
+    window.addEventListener("scroll", handleScroll)
+
+    return () => {
+      clearTimeout(timeout)
+      window.removeEventListener("scroll", handleScroll)
+    }
+  }, [])
+  const isUpScrolled = scrollDirection === "up" && !scrolledToTop
+  const isDownScrolled = scrollDirection === "down" && !scrolledToTop
+
+  return (
+    <header
+      className={`Header ${isUpScrolled ? "Header--scrollUp" : ""} ${
+        isDownScrolled ? "Header--scrollDown" : ""
+      }`}
+    >
+      <div className="Header__container">
+        <h1 className="Header__logo">
+          <Link className="Logo" to="/">
+            <IconLogo />
+          </Link>
+        </h1>
+        <Navbar />
+      </div>
+    </header>
+  )
+}
